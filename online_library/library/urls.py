1701c8150553238f2f65f026ae84cfa0f682acf4
Invader6666 <aefranko123@gmail.com>
2026-05-06 02:27:43 +0300

    Search implementation
    -Added search functionality

--- online_library/library/urls.py
+++ online_library/library/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('search/', views.search, name='search'),
+    path('borrowed_books/', views.borrowed_books, name='borrowed_books'),
+    path('user_books/', views.user_books, name='user_books'),
 ]
--- online_library/library/views.py
+++ online_library/library/views.py
@@ -1,4 +1,39 @@
 from django.shortcuts import render
+from .models import Book
 
 def home(request):
     return render(request, 'index.html')
+
+def borrowed_books(request):
+    return render(request, 'borrowed_books.html')
+
+def user_books(request):
+    return render(request, 'user_books.html')
+
+def search(request):
+    title = request.GET.get('title', '')
+    author = request.GET.get('author', '')
+    category = request.GET.get('category', 'Any')
+    availability = request.GET.get('availability', 'Any')
+
+    books = Book.objects.all()
+
+    if title:
+        books = books.filter(title__icontains=title)
+
+    if author:
+        books = books.filter(author__icontains=author)
+
+    if category != 'Any':
+        books = books.filter(category=category)
+
+    if availability != 'Any':
+        books = books.filter(availability=availability)
+
+    return render(request, 'search.html', {
+        'books': books,
+        'title': title,
+        'author': author,
+        'category': category,
+        'availability': availability,
+    })
