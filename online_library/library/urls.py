fe7318dda3a3780dd6eb47feb2700e234976aed7
Ahmed Bakr Abd El Hafez <20250907@stud.fci-cu.edu.eg>
2026-05-07 02:33:10 +0300

    Change user_books URL path to books

--- online_library/library/urls.py
+++ online_library/library/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('user_books/', views.user_books, name='user_books'),
+    path('books/', views.user_books, name='user_books'),
+    
 ]
