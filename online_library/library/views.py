9b55fed7ce3dcbf7b631b453888e39b011f6d66e
Invader6666 <aefranko123@gmail.com>
2026-05-08 02:21:51 +0300

    FIXES
    -login logout session fix

--- online_library/library/views.py
+++ online_library/library/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
