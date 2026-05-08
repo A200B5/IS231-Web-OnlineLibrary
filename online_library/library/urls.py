0f42a7308dafd88bc6eb4b0dde1bdc63da06b2c8
Mohamed <mm6920@fayoum.edu.eg>
2026-05-08 12:13:43 +0300

    fix admin_dashboard

--- online_library/library/urls.py
+++ online_library/library/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     path('admin/increase_copies/<int:id>/', views.increase_copies, name='increase_copies'),
     path('admin/decrease_copies/<int:id>/', views.decrease_copies, name='decrease_copies'),
     path('admin/delete_book/<int:id>/', views.delete_book, name='delete_book'),
+    path('admin/add_book/', views.admin_add_book, name='admin_add_book'),
+    path('admin/edit_book/<int:id>/', views.admin_edit_book, name='admin_edit_book'),
 ]
